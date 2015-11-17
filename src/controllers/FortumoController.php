f5221f639e392783ebd57bc96686083cb848fde9
Ronalds Sovas <sovas@greenbox.lv>
2015-11-17 22:18:42 +0200

    added log inside

--- src/controllers/FortumoController.php
+++ src/controllers/FortumoController.php
@@ -7,21 +7,23 @@ use Illuminate\Http\Request;
 use \Carbon;
 
 use \Config;
+use \Log;
 
 
 class FortumoController extends \Shivergard\Fortumo\PackageController {
 
-	public function test(){
-		return false;
-	}
+    public function test(){
+        return false;
+    }
 
-	public function billing(){
-		return false;
-	}
+    public function billing(){
+        return false;
+    }
 
 
-	public function init(){
-		return \Shivergard\Fortumo\Fortumo::get();
-	}
+    public function init(){
+        Log::info("Hello [".$_SERVER['REMOTE_ADDR']."]");
+        return \Shivergard\Fortumo\Fortumo::get();
+    }
 
 }
